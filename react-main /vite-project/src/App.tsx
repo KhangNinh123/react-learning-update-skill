9947d6a6928e458bed8c1ccfa9e205a7078085c6
KhangNinh123 <ninhtrinh744@gmail.com>
2026-07-01 13:57:37 +0700

    Cập nhật React State là OJ

--- react-main /vite-project/src/App.tsx
+++ react-main /vite-project/src/App.tsx
@@ -1,122 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+//shadow clone
+export default function App() {
+  const [person, setPerson] = useState({
+    name: 'Dư Thanh Được',
+    position: {
+      title: 'Giám đốc',
+      salary: 1000000,
+    },
+  })
+  //Khi set lai
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div className="p-8">
+      <form className="flex flex-col gap-4 max-w-sm">
+        <input
+          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
+          value={person.name}
+          name="name"
+          onChange={(e) => {
+            setPerson({
+              ...person,
+              name: e.target.value,
+            })
+          }}
+        />
+        <input
+          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
+          value={person.position.title}
+          name="title"
+          onChange={(e) => {
+            setPerson({
+              ...person,
+              position: {
+                ...person.position,
+                title: e.target.value,
+              },
+            })
+          }}
+        />
+        <input
+          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
+          value={person.position.salary}
+          name="salary"
+          onChange={(e) => {
+            setPerson({
+              ...person,
+              position: {
+                ...person.position,
+                salary: parseInt(e.target.value),
+              },
+            })
+          }}
+        />
+      </form>
+    </div>
   )
 }
-
-export default App
