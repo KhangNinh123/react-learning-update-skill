91d6d1013b43282ee8cd57f8114e5768619fb4ab
KhangNinh123 <ninhtrinh744@gmail.com>
2026-07-01 14:31:09 +0700

    Cập nhật React State là Arrray

--- react-main /vite-project/src/App.tsx
+++ react-main /vite-project/src/App.tsx
@@ -1,57 +1,46 @@
 import { useState } from 'react'
-//shadow clone
+import './App.css'
+
+let nextId = 0;
+
+type Artist = {
+  id: number
+  name: string
+}
+
 export default function App() {
-  const [person, setPerson] = useState({
-    name: 'Dư Thanh Được',
-    position: {
-      title: 'Giám đốc',
-      salary: 1000000,
-    },
-  })
-  //Khi set lai
+  const [name, setName] = useState('')
+  const [artists, setArtists] = useState<Artist[]>([])
+
+  const handleRemoveArtist = (artistId: number) => {
+    const newArtists = artists.filter((artist) => artist.id !== artistId)
+    setArtists(newArtists)
+  }
+
+  console.log(artists)
   return (
-    <div className="p-8">
-      <form className="flex flex-col gap-4 max-w-sm">
-        <input
-          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
-          value={person.name}
-          name="name"
-          onChange={(e) => {
-            setPerson({
-              ...person,
-              name: e.target.value,
-            })
-          }}
-        />
-        <input
-          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
-          value={person.position.title}
-          name="title"
-          onChange={(e) => {
-            setPerson({
-              ...person,
-              position: {
-                ...person.position,
-                title: e.target.value,
-              },
-            })
-          }}
-        />
-        <input
-          className="border border-gray-400 p-2 rounded outline-none focus:border-blue-500"
-          value={person.position.salary}
-          name="salary"
-          onChange={(e) => {
-            setPerson({
-              ...person,
-              position: {
-                ...person.position,
-                salary: parseInt(e.target.value),
-              },
-            })
-          }}
-        />
-      </form>
-    </div>
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          const newArtists = [ ...artists, { id: nextId++, name }]
+          setArtists(newArtists)
+          setName('')
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            {artist.name}{' '}
+            <button onClick={() => handleRemoveArtist(artist.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
